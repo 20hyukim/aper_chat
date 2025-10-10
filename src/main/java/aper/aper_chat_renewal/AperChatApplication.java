79bc362e8aef41352eea57487fa06305eb026805
20hyukim <20hyukim@krafton.com>
2025-10-10 16:53:12 +0900

    feat: init commit

---
+++ src/main/java/aper/aper_chat_renewal/AperChatApplication.java
@@ -0,0 +1,13 @@
+package aper.aper_chat_renewal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AperChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AperChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aper/aper_chat_renewal/AperChatRenewalApplicationTests.java
@@ -0,0 +1,13 @@
+package aper.aper_chat_renewal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AperChatRenewalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
